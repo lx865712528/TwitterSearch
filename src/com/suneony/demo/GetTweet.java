74a9987cbbdcf5093a9d24f659d67987b63930e9
unknown <liuxiao@youdao.internal>
2016-04-15 11:37:24 +0800

    MOD: modify

--- src/com/suneony/demo/GetTweet.java
+++ src/com/suneony/demo/GetTweet.java
@@ -8,34 +8,31 @@ import java.io.OutputStreamWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Created by liuxiao on 2016/4/15 0015.
  */
 public class GetTweet {
 
-
-    public static String getStr(Integer dd) {
-        try {
-            Date date = new SimpleDateFormat("yyyyMMdd").parse(dd.toString());
-            return new SimpleDateFormat("yyyy-MM-dd").format(date);
-        } catch (ParseException e) {
-            return "2011-01-01";
-        }
-    }
-
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, ParseException {
         SearchFactory sf = new SearchFactory();
-        Integer start = 20110101;
-        Integer end = 20120101;
+        Date start = new SimpleDateFormat("yyyyMMdd").parse("20110101");
+        Date end = new SimpleDateFormat("yyyyMMdd").parse("20120101");
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(start);
         int cc = 0;
-        while (start < end) {
-            ArrayList<String> tweetListOfKeyword = (ArrayList<String>) sf.search("\"paper accepted since:" + getStr(start) + " until:" + getStr(start + 1) + "\"");
-            OutputStreamWriter outputStream = new OutputStreamWriter(new FileOutputStream("data/" + start + ".json"));
-            for (String string : tweetListOfKeyword) {
-                outputStream.write(string + "\n");
-            }
+        while (start.getTime() < end.getTime()) {
+            String fileName = new SimpleDateFormat("yyyyMMdd").format(start) + ".json";
+            String keyword = "paper accepted since:" + new SimpleDateFormat("yyyy-MM-dd").format(start) + " until:";
+            calendar.add(calendar.DATE, 1);
+            start = calendar.getTime();
+            keyword += new SimpleDateFormat("yyyy-MM-dd").format(start);
+            ArrayList<String> tweetListOfKeyword = (ArrayList<String>) sf.search("\"" + keyword + "\"");
+            OutputStreamWriter outputStream = new OutputStreamWriter(new FileOutputStream("data/" + fileName));
+            for (String string : tweetListOfKeyword) outputStream.write(string + "\n");
             System.out.print(start + "done!");
             ++cc;
             outputStream.close();
@@ -43,7 +40,6 @@ public class GetTweet {
                 cc = 0;
                 Thread.sleep(900000);
             }
-            ++start;
         }
         System.out.println("Done!");
     }
